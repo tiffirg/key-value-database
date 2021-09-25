393506eec6be417a786672d2f7165a1986cfaa78
Vadim Sannikov <vadim.sannikov.a@gmail.com>
2021-09-25 11:44:30 +0300

    Added new funcs for report

--- src/main/kotlin/services/FuncsForReport.kt
+++ src/main/kotlin/services/FuncsForReport.kt
@@ -11,4 +11,10 @@ fun printHelp() = println(
 
 fun printNotExistFile(nameFile: String) = println("$nameFile: No such file or directory")
 
-fun printInvalidKey(key: String) = println("$key: No such database key")
+fun printInvalidKey(db: String, key: String) = println("$db: No such database key $key")
+
+fun printKeySizeExceeded(key: String) = println("$key: Key size exceeded")
+
+fun printInvalidString(str: String) = println("$str: Incorrect entry")
+
+fun printGetResponseDB(db: String, key: String, value: String) = println("$db: $key - $value")
